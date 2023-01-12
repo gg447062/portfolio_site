c9d4e516c619419225bde6db4e6e4519692c82ef
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-01-12 15:44:33 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
