fdd4704a6f5e21b540fc3da7f6b2c015620124a9
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-01-14 16:13:48 -0600

    adds modals for project links

--- public/main.js
+++ public/main.js
@@ -13,13 +13,29 @@ function makeSpans() {
 }
 
 function randomizeFontSize(min, max) {
-  const spans = document.querySelectorAll('span');
-  spans.forEach((span) => {
+  document.querySelectorAll(['h1', 'h2']).forEach((el) => {
     const randomVal = Math.random() * (max - min) + min;
-    span.style.fontSize = `${randomVal.toFixed(1)}em`;
+    el.style.fontSize = `${randomVal.toFixed(1)}em`;
   });
+}
+
+function showModal(e) {
+  document.getElementById(`${e.target.id}-modal`).classList.add('active');
+}
+
+function hideModal(e) {
+  document.getElementById(`${e.target.id}-modal`).classList.remove('active');
 }
 
 makeSpans();
 
-randomizeFontSize(2, 7);
+randomizeFontSize(9, 11);
+
+document.querySelectorAll('a.project-link').forEach((link) => {
+  link.addEventListener('mouseenter', (e) => {
+    showModal(e);
+  });
+  link.addEventListener('mouseleave', (e) => {
+    hideModal(e);
+  });
+});
