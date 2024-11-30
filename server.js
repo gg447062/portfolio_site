836d6332a265bece03520e3a977681b309317274
Gabriel Gutierrez <77996586+gg447062@users.noreply.github.com>
2024-11-30 00:49:00 -0700

    adds projects.json and updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/projects', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./projects.json'));
+  res.json(data);
+});
 
 // app.use('/', (req, res) => {
 //   res.sendFile(__dirname + '/public/index.html');
